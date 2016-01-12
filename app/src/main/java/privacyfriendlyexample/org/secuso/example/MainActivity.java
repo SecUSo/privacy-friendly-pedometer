6fae9a56d89c11d6077a325925559b1b1ea2f516
Tobias Neidig <git@tobias-neidig.de>
2016-01-12 22:45:22 +0100

    Basic structure added

--- app/src/main/java/privacyfriendlyexample/org/secuso/example/MainActivity.java
+++ app/src/main/java/privacyfriendlyexample/org/secuso/example/MainActivity.java
@@ -1,37 +1,200 @@
 package privacyfriendlyexample.org.secuso.example;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private String activityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.app_name);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#024265")));
+
+        drawerList = (ListView) findViewById(R.id.navList);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        activityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[2];
+
+        drawerItem[0] = new ObjectDrawerItem(R.drawable.ic_action_help, getString(R.string.action_help), "");
+        drawerItem[1] = new ObjectDrawerItem(R.drawable.ic_action_about, getString(R.string.action_about), "");
+
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
+        drawerList.setAdapter(adapter);
+    }
+
+    private void addDrawerItems() {
+        String[] mNavigationDrawerItemTitles = {getString(R.string.action_help), getString(R.string.action_about)};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNavigationDrawerItemTitles);
+        drawerList.setAdapter(adapter);
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.action_navigation);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //Remove comment in case menu on the right is needed
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                //fragment = new EnterPinFragment();
+                break;
+            case 1:
+                //fragment = new NumpadFragment();
+                break;
+            case 2:
+                //fragment = new HelpFragment();
+                break;
+            case 3:
+                //fragment = new AboutFragment();
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).addToBackStack(null).commit();
+
+            drawerList.setItemChecked(position, true);
+            drawerList.setSelection(position);
+            drawerLayout.closeDrawer(drawerList);
+
+        } else {
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+    }
+
+    public class DrawerItemCustomAdapter extends ArrayAdapter<ObjectDrawerItem> {
+
+        Context mContext;
+        int layoutResourceId;
+        ObjectDrawerItem data[] = null;
+
+        public DrawerItemCustomAdapter(Context mContext, int layoutResourceId, ObjectDrawerItem[] data) {
+
+            super(mContext, layoutResourceId, data);
+            this.layoutResourceId = layoutResourceId;
+            this.mContext = mContext;
+            this.data = data;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View listItem = convertView;
+
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            listItem = inflater.inflate(layoutResourceId, parent, false);
+
+            ImageView imageViewIcon = (ImageView) listItem.findViewById(R.id.imageViewIcon);
+            TextView textViewName = (TextView) listItem.findViewById(R.id.textViewName);
+            TextView textViewDescription = (TextView) listItem.findViewById(R.id.textViewDescription);
+
+            ObjectDrawerItem folder = data[position];
+
+            imageViewIcon.setImageResource(folder.icon);
+            textViewName.setText(folder.name);
+            textViewDescription.setText(folder.description);
+
+            return listItem;
+        }
+
     }
 }
---
+++ app/src/main/java/privacyfriendlyexample/org/secuso/example/ObjectDrawerItem.java
@@ -0,0 +1,19 @@
+package privacyfriendlyexample.org.secuso.example;
+
+/**
+ * Created by yonjuni on 12.01.16.
+ */
+public class ObjectDrawerItem {
+
+    public int icon;
+    public String name;
+    public String description;
+
+    public ObjectDrawerItem(int icon, String name, String description) {
+
+        this.icon = icon;
+        this.name = name;
+        this.description = description;
+    }
+
+}

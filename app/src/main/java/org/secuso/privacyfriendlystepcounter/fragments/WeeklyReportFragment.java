3885aa35e38d55e17590333e0939323fe1239144
Tobias Neidig <git@tobias-neidig.de>
2016-07-27 12:04:56 +0200

    Show summary for week and month

--- app/src/main/java/org/secuso/privacyfriendlystepcounter/fragments/WeeklyReportFragment.java
+++ app/src/main/java/org/secuso/privacyfriendlystepcounter/fragments/WeeklyReportFragment.java
@@ -1,20 +1,44 @@
 package org.secuso.privacyfriendlystepcounter.fragments;
 
+import android.app.DatePickerDialog;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
+import org.secuso.privacyfriendlystepcounter.Factory;
 import org.secuso.privacyfriendlystepcounter.R;
 import org.secuso.privacyfriendlystepcounter.adapters.ReportAdapter;
+import org.secuso.privacyfriendlystepcounter.models.ActivityChartDataSet;
+import org.secuso.privacyfriendlystepcounter.models.ActivityDayChart;
 import org.secuso.privacyfriendlystepcounter.models.ActivitySummary;
+import org.secuso.privacyfriendlystepcounter.models.StepCount;
+import org.secuso.privacyfriendlystepcounter.models.WalkingMode;
+import org.secuso.privacyfriendlystepcounter.persistence.StepCountPersistenceHelper;
+import org.secuso.privacyfriendlystepcounter.persistence.WalkingModePersistenceHelper;
+import org.secuso.privacyfriendlystepcounter.services.AbstractStepDetectorService;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Report-fragment for one specific day
@@ -28,13 +52,34 @@ import java.util.Arrays;
  * @author Tobias Neidig
  * @version 20160606
  */
-public class WeeklyReportFragment extends Fragment {
+public class WeeklyReportFragment extends Fragment implements ReportAdapter.OnItemClickListener {
+    public static String LOG_TAG = WeeklyReportFragment.class.getName();
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ReportAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     private OnFragmentInteractionListener mListener;
+
+    private Calendar day;
+    private ActivitySummary activitySummary;
+    private ActivityDayChart activityChart;
+    private List<Object> reports = new ArrayList<>();
+
+    private AbstractStepDetectorService.StepDetectorBinder myBinder;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            myBinder = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myBinder = (AbstractStepDetectorService.StepDetectorBinder) service;
+            generateReports(true);
+        }
+    };
 
     public WeeklyReportFragment() {
         // Required empty public constructor
@@ -72,7 +117,10 @@ public class WeeklyReportFragment extends Fragment {
 
         // specify an adapter
         // using sample data.
-        mAdapter = new ReportAdapter(new ArrayList<>(Arrays.asList(new Object[]{new ActivitySummary(4200 * 7, 42 * 7, 420 * 7, "06. - 12. Juni")})));
+        day = Calendar.getInstance();
+        generateReports(false);
+        mAdapter = new ReportAdapter(reports);
+        mAdapter.setOnItemClickListener(this);
         mRecyclerView.setAdapter(mAdapter);
 
         // use a linear layout manager
@@ -82,6 +130,12 @@ public class WeeklyReportFragment extends Fragment {
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
+
+        // Bind to stepDetector if today is shown
+        if (isTodayShown()) {
+            Intent serviceIntent = new Intent(getContext(), Factory.getStepDetectorServiceClass(getContext().getPackageManager()));
+            getContext().bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+        }
 
         return view;
     }
@@ -102,6 +156,147 @@ public class WeeklyReportFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
+    /**
+     * @return is the day which is currently shown today?
+     */
+    private boolean isTodayShown() {
+        // TODO update to week
+        return (Calendar.getInstance().get(Calendar.YEAR) == day.get(Calendar.YEAR) &&
+                Calendar.getInstance().get(Calendar.MONTH) == day.get(Calendar.MONTH) &&
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH) == day.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * Generates the report objects and adds them to the recycler view adapter.
+     * The following reports will be generated:
+     * * ActivitySummary
+     * * ActivityChart
+     * If one of these reports does not exist it will be created and added at the end of view.
+     *
+     * @param updated determines if the method is called because of an update of current steps.
+     *                If set to true and another day than today is shown the call will be ignored.
+     */
+    private void generateReports(boolean updated) {
+        Log.i(LOG_TAG, "Generating reports");
+        if (!this.isTodayShown() && updated || isDetached() || getContext() == null) {
+            // the day shown is not today or is detached
+            return;
+        }
+        // Get all step counts for this day.
+        day.set(Calendar.DAY_OF_WEEK, day.getFirstDayOfWeek());
+        Calendar start = (Calendar) day.clone();
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM", getResources().getConfiguration().locale);
+        Map<String, Double> stepMap = new HashMap<>();
+        Map<String, Double> distanceMap = new HashMap<>();
+        Map<String, Double> caloriesMap = new HashMap<>();
+        int totalSteps = 0;
+        double totalDistance = 0;
+        int totalCalories = 0;
+        for(int i = 0; i < 7; i ++){
+            List<StepCount> stepCounts = StepCountPersistenceHelper.getStepCountsForDay(start, getContext());
+            // TODO add current steps if today
+            int steps = 0;
+            double distance = 0;
+            int calories = 0;
+            for(StepCount stepCount : stepCounts){
+                steps += stepCount.getStepCount();
+                distance += (stepCount.getDistance())/1000; // convert from m to km
+                calories += stepCount.getCalories();
+            }
+            stepMap.put(formatDate.format(start.getTime()), (double) steps);
+            distanceMap.put(formatDate.format(start.getTime()), distance);
+            caloriesMap.put(formatDate.format(start.getTime()), (double) calories);
+            totalSteps += steps;
+            totalDistance += distance;
+            totalCalories += calories;
+            if(i != 6) {
+                start.add(Calendar.DAY_OF_MONTH, 1);
+            }
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.", getResources().getConfiguration().locale);
+        SimpleDateFormat simpleDateMonthFormat = new SimpleDateFormat("dd. MMMM", getResources().getConfiguration().locale);
+
+        String title = simpleDateFormat.format(day.getTime()) + " - " + simpleDateMonthFormat.format(start.getTime());
+
+        // create view models
+        if (activitySummary == null) {
+            activitySummary = new ActivitySummary(totalSteps, totalDistance, totalCalories, title);
+            reports.add(activitySummary);
+        } else {
+            activitySummary.setSteps(totalSteps);
+            activitySummary.setDistance(totalDistance);
+            activitySummary.setCalories(totalCalories);
+            activitySummary.setTitle(title);
+        }
+/*
+        if (activityChart == null) {
+            activityChart = new ActivityDayChart(stepData, distanceData, caloriesData, simpleDateFormat.format(day.getTime()));
+            activityChart.setDisplayedDataType(ActivityDayChart.DataType.STEPS);
+            reports.add(activityChart);
+        } else {
+            activityChart.setSteps(stepData);
+            activityChart.setDistance(distanceData);
+            activityChart.setCalories(caloriesData);
+            activityChart.setTitle(simpleDateFormat.format(day.getTime()));
+        }
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String d = sharedPref.getString(getString(R.string.pref_daily_step_goal), "10000");
+        activityChart.setGoal(Integer.valueOf(d));
+*/
+
+        // notify ui
+        if (mAdapter != null) {
+            mAdapter.notifyItemChanged(reports.indexOf(activitySummary));
+            //mAdapter.notifyItemChanged(reports.indexOf(activityChart));
+            mAdapter.notifyDataSetChanged();
+        } else {
+            Log.w(LOG_TAG, "Cannot inform adapter for changes.");
+        }
+    }
+
+    @Override
+    public void onActivityChartDataTypeClicked(ActivityDayChart.DataType newDataType) {
+
+    }
+
+    @Override
+    public void setActivityChartDataTypeChecked(Menu popup) {
+
+    }
+
+    @Override
+    public void onPrevClicked() {
+        this.day.add(Calendar.WEEK_OF_YEAR, -1);
+        this.generateReports(false);
+    }
+
+    @Override
+    public void onNextClicked() {
+        this.day.add(Calendar.WEEK_OF_YEAR, 1);
+        this.generateReports(false);
+    }
+
+    @Override
+    public void onTitleClicked() {
+        int year = this.day.get(Calendar.YEAR);
+        int month = this.day.get(Calendar.MONTH);
+        int day = this.day.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        DatePickerDialog dialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                WeeklyReportFragment.this.day.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                WeeklyReportFragment.this.day.set(Calendar.MONTH, monthOfYear);
+                WeeklyReportFragment.this.day.set(Calendar.YEAR, year);
+                WeeklyReportFragment.this.generateReports(false);
+            }
+        }, year, month, day);
+        dialog.show();
+    }
+
 
     /**
      * This interface must be implemented by activities that contain this

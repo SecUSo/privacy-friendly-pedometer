b8df9bd772e881c9f3533e434fb1623050e6fadb
Tobias Neidig <git@tobias-neidig.de>
2016-07-29 20:22:37 +0200

    Updated about view
    * Added third party library notes
    * Made about text clickable

--- app/src/main/java/org/secuso/privacyfriendlystepcounter/fragments/AboutFragment.java
+++ app/src/main/java/org/secuso/privacyfriendlystepcounter/fragments/AboutFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +31,10 @@ public class AboutFragment extends Fragment {
         }
         TextView versionTextView = (TextView) rootView.findViewById(R.id.textFieldVersion);
         versionTextView.setText(String.format(getString(R.string.version_number), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
+        TextView urlTextView = (TextView) rootView.findViewById(R.id.textViewUrl);
+        urlTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView libTextView = (TextView) rootView.findViewById(R.id.textViewLib);
+        libTextView.setMovementMethod(LinkMovementMethod.getInstance());
         container.removeAllViews();
         return rootView;
     }

8bb1e02ff2b3876bde06022b64374772da8a8589
Karola Marky <yonjuni@pinyto.de>
2016-11-13 13:58:42 +0200

    Clean up, AboutActivity plus layout added to structure

---
+++ app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/AboutActivity.java
@@ -0,0 +1,43 @@
+package org.secuso.privacyfriendlyactivitytracker.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import org.secuso.privacyfriendlyactivitytracker.R;
+
+
+/**
+ * Created by yonjuni on 15.06.16.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar ab = getSupportActionBar();
+        if(ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        View mainContent = findViewById(R.id.main_content);
+        if (mainContent != null) {
+            mainContent.setAlpha(0);
+            mainContent.animate().alpha(1).setDuration(BaseActivity.MAIN_CONTENT_FADEIN_DURATION);
+        }
+
+        overridePendingTransition(0, 0);
+
+        ((TextView)findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    //@Override
+    //protected int getNavigationDrawerID() {
+    //    return R.id.nav_about;
+    //}
+}
---
+++ app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/BaseActivity.java
@@ -0,0 +1,8 @@
+package org.secuso.privacyfriendlyactivitytracker.activities;
+
+/**
+ * Created by karo on 11.11.16.
+ */
+
+public class BaseActivity {
+}

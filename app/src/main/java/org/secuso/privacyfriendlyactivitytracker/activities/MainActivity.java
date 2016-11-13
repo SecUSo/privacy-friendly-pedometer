89178ef4d67373189fc1ece4644ec607925bcdf2
Karola Marky <yonjuni@pinyto.de>
2016-11-13 15:45:51 +0200

    Try to add PFA design

--- app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/MainActivity.java
@@ -35,12 +35,12 @@ public class MainActivity extends BaseActivity implements DailyReportFragment.On
         PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
 
         // show welcome dialog on first run
-//        String isNotFirstRun = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.pref_is_not_first_run), "");
-//        if (isNotFirstRun.equals("")) {
-//            PreferenceManager.getDefaultSharedPreferences(this).edit().putString(getString(R.string.pref_is_not_first_run), "true").apply();
-//            WelcomeDialog welcomeDialog = new WelcomeDialog();
-//            welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
-//        }
+        String isNotFirstRun = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.pref_is_not_first_run), "");
+        if (isNotFirstRun.equals("")) {
+            PreferenceManager.getDefaultSharedPreferences(this).edit().putString(getString(R.string.pref_is_not_first_run), "true").apply();
+            WelcomeDialog welcomeDialog = new WelcomeDialog();
+            welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
+        }
 
 
 

29aa3b7544df37233a7a475b91c0a96554b457a4
Karola Marky <yonjuni@pinyto.de>
2016-12-14 22:42:36 +0200

    Welcome dialog replaced with tutorial

--- app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyactivitytracker/activities/MainActivity.java
@@ -1,14 +1,7 @@
 package org.secuso.privacyfriendlyactivitytracker.activities;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.LayoutInflater;
-import android.view.Menu;
 
 import org.secuso.privacyfriendlyactivitytracker.R;
 import org.secuso.privacyfriendlyactivitytracker.fragments.DailyReportFragment;
@@ -20,8 +13,8 @@ import org.secuso.privacyfriendlyactivitytracker.utils.StepDetectionServiceHelpe
 /**
  * Main view incl. navigation drawer and fragments
  *
- * @author Tobias Neidig
- * @version 20160601
+ * @author Tobias Neidig, Karola Marky
+ * @version 20161214
  */
 public class MainActivity extends BaseActivity implements DailyReportFragment.OnFragmentInteractionListener, WeeklyReportFragment.OnFragmentInteractionListener, MonthlyReportFragment.OnFragmentInteractionListener {
 
@@ -34,14 +27,6 @@ public class MainActivity extends BaseActivity implements DailyReportFragment.On
         // init preferences
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
         PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
-
-        // show welcome dialog on first run
-        String isNotFirstRun = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.pref_is_not_first_run), "");
-        if (isNotFirstRun.equals("")) {
-            PreferenceManager.getDefaultSharedPreferences(this).edit().putString(getString(R.string.pref_is_not_first_run), "true").apply();
-            WelcomeDialog welcomeDialog = new WelcomeDialog();
-            welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
-        }
 
         // Load first view
         final android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -58,32 +43,4 @@ public class MainActivity extends BaseActivity implements DailyReportFragment.On
         return R.id.menu_home;
     }
 
-    public static class WelcomeDialog extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            LayoutInflater i = getActivity().getLayoutInflater();
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setView(i.inflate(R.layout.welcome_dialog, null));
-            builder.setIcon(R.mipmap.ic_drawer);
-            builder.setTitle(getActivity().getString(R.string.welcome));
-            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
-            builder.setNegativeButton(getActivity().getString(R.string.view_help), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent intent = new Intent(getActivity(), HelpActivity.class);
-                    startActivity(intent);
-                }
-            });
-            builder.setNeutralButton(getActivity().getString(R.string.view_personal_settings), new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    Intent intent = new Intent(getActivity(), PreferencesActivity.class);
-                    intent.putExtra(PreferencesActivity.EXTRA_SHOW_FRAGMENT, PreferencesActivity.GeneralPreferenceFragment.class.getName());
-                    intent.putExtra(PreferencesActivity.EXTRA_SHOW_FRAGMENT_TITLE, R.string.action_settings);
-                    startActivity(intent);
-                }
-            });
-
-            return builder.create();
-        }
-    }
 }

be96fe1830e9104f8b9eef561d639aa333beca5f
Tobias Neidig <git@tobias-neidig.de>
2017-09-02 09:46:57 +0200

    Add more help sections

--- app/src/main/java/org/secuso/privacyfriendlyactivitytracker/helpers/HelpDataDump.java
+++ app/src/main/java/org/secuso/privacyfriendlyactivitytracker/helpers/HelpDataDump.java
@@ -44,33 +44,43 @@ public class HelpDataDump {
         List<String> general = new ArrayList<>();
         general.add(context.getResources().getString(R.string.help_intro));
         general.add(context.getResources().getString(R.string.help_functionality_step_counter_summary));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_overview_heading), general);
 
         List<String> notifications = new ArrayList<>();
         notifications.add(context.getResources().getString(R.string.help_functionality_notification_summary));
         notifications.add(context.getResources().getString(R.string.help_functionality_notification_delete));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_functionality_notification), notifications);
 
         List<String> motivationalert = new ArrayList<>();
         motivationalert.add(context.getResources().getString(R.string.help_functionality_motivation_alert_summary));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_functionality_motivation_alert), motivationalert);
+
+        List<String> walkingModes = new ArrayList<>();
+        walkingModes.add(context.getResources().getString(R.string.help_walkingmodes_selection));
+        expandableListDetail.put(context.getResources().getString(R.string.help_walkingmodes_heading), walkingModes);
+
+        List<String> training = new ArrayList<>();
+        training.add(context.getResources().getString(R.string.help_training_selection));
+        expandableListDetail.put(context.getResources().getString(R.string.help_training_heading), training);
+
+        List<String> distancemeasurement = new ArrayList<>();
+        distancemeasurement.add(context.getResources().getString(R.string.help_distancemeasurement_selection));
+        expandableListDetail.put(context.getResources().getString(R.string.help_distancemeasurement_heading), distancemeasurement);
+
+        List<String> correctsteps = new ArrayList<>();
+        correctsteps.add(context.getResources().getString(R.string.help_correctsteps_selection));
+        expandableListDetail.put(context.getResources().getString(R.string.help_correctsteps_heading), correctsteps);
 
         List<String> permissions = new ArrayList<>();
         permissions.add(context.getResources().getString(R.string.help_permissions_selection));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_privacy_heading), permissions);
 
         List<String> bootpermission = new ArrayList<>();
         bootpermission.add(context.getResources().getString(R.string.help_permission_boot_description));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_permission_boot_heading), bootpermission);
 
         List<String> wakePermission = new ArrayList<>();
         wakePermission.add(context.getResources().getString(R.string.help_permission_wake_description));
-
         expandableListDetail.put(context.getResources().getString(R.string.help_permission_wake_heading), wakePermission);
 
         return expandableListDetail;
